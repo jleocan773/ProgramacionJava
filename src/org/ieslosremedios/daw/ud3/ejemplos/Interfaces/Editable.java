2201b5ce8803db2d733afb0d773d2b057191233c
Jonathan Leon Canto <jleocan773@g.educaand.es>
2023-01-11 10:29:55 +0100

    Ejercicio 3.4 Interfaces

--- src/org/ieslosremedios/daw/ud3/ejemplos/Interfaces/Editable.java
+++ src/org/ieslosremedios/daw/ud3/ejemplos/Interfaces/Editable.java
@@ -1,13 +1,13 @@
 package org.ieslosremedios.daw.ud3.ejemplos.Interfaces;
 
 /**8.- Crea una nueva interfaz llamada “Editable” y traspasa (corta y pega) el método “establecerContenido” a ella.
-//Luego haz que “Imprimible” herede tanto de “Borrable” como de “Imprimible”.
+//Luego haz que “Imprimible” herede tanto de “Borrable” como de “Editable”.
 
 //¿Sigue funcionando igual? ¿Por qué?
 
  Sí, sigue funcionando igual, porque es posible que una interfaz herede de varias interfaces, al contrario que una clase, que solo puede heredar de una */
 
-public interface Editable extends Borrable, Imprimible{
+public interface Editable {
     void establecerContenido(String contenido); //8.- Traspasa, es decir, corta y pega el método “establecerContenido”
 
 }
--- src/org/ieslosremedios/daw/ud3/ejemplos/Interfaces/Imprimible.java
+++ src/org/ieslosremedios/daw/ud3/ejemplos/Interfaces/Imprimible.java
@@ -1,6 +1,7 @@
 package org.ieslosremedios.daw.ud3.ejemplos.Interfaces;
 
-public interface Imprimible extends Borrable{ //1.- Creamos la interfaz "Imprimible"
+//8.- Haz que “Imprimible” herede tanto de “Borrable” como de “Editable”.
+public interface Imprimible extends Borrable, Editable{ //1.- Creamos la interfaz "Imprimible"
     String TEXTO_POR_DEFECTO = "Página de Prueba"; //2.- Creamos e inicializamos la constante "TEXT_POR_DEFECTO" con el valor "Página de prueba"
     void Imprime(); //1.- Definimos un método, que no devuelve nada y no tiene parámetros
 
