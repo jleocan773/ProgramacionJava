40ef0aae3cadb9b0eaa3a8cd8f6ec282024e1b14
Jonathan Leon Canto <jleocan773@g.educaand.es>
2023-01-26 13:16:01 +0100

    Ejercicio 4.3 Me falta hacer el TODO 1

--- src/org/ieslosremedios/daw/ud4/ejercicios/actividad4_3/Main.java
+++ src/org/ieslosremedios/daw/ud4/ejercicios/actividad4_3/Main.java
@@ -13,7 +13,6 @@ public class Main {
         int[] yEsteOtroArrayParaClonar = {5,10,15,20,25,30}; //Creo un array que utilizaré mñas tarde en el apartado 6
 
 
-
         for (int i = 0; i < arrayAleatorio.length; i++) { //El contenido se lo asignará con un bucle for
             arrayAleatorio[i] = numaleatorio.nextInt(100); //Usando el método "nextInt()", que crea un número aleatorio, asigno a todas las posiciones de "arrayAleatorio"
         }
@@ -22,32 +21,27 @@ public class Main {
 
         //TODO 2.- Imprime el array anterior empezando por la última posición
         System.out.println("2.- Imprime el array anterior empezando por la última posición");
-        for (int i = arrayAleatorio.length - 1; i >= 0; i--){ //Mientras que "i" sea menor que la longitud del array-1 (uno), se hará lo del bucle y se le
-                                                              // restará 1 (uno) a "i", así empezará por la última posición hasta llegar a la primera, la 0 (cero)
-            System.out.print(arrayAleatorio[i] + " ");        //Mostramos la posición pertinente
-        }
+        System.out.println(invertirArray(arrayAleatorio));
 
         System.out.println("\n---------------------------------------------------------");
 
         //TODO 3.- Ordena el array utilizando la clase de utilidades Arrays
         System.out.println("3.- Ordena el array utilizando la clase de utilidades Arrays");
-        Arrays.sort(arrayAleatorio); //Usando el método "sort" ordenamos arrayAleatorio
-        System.out.println(Arrays.toString(arrayAleatorio));
+        System.out.println(ordenarArray(arrayAleatorio));
 
         System.out.println("---------------------------------------------------------");
 
         //TODO 4.- Busca un número recibido como parámetro dentro del array. Por dos métodos diferentes, primero el manual y luego el binarySearch
         System.out.println("4.- Busca un número recibido como parámetro dentro del array. Por dos métodos diferentes, primero el manual y luego el binarySearch");
-        System.out.println("Búsqueda manual: El número que buscas está en la posición -> " + buscarEnArray(arrayAleatorio,50)); //Usamos el método creado
-        System.out.println("Búsqueda de binarySearch: El número que buscas está en la posición -> " + Arrays.binarySearch(arrayAleatorio,50));
+        System.out.println(buscarEnArray(arrayAleatorio,50)); //Usamos el método creado
+        System.out.println(busquedaBinaria(arrayAleatorio, 50)); //Usamos el método que usa BinarySearch
 
         System.out.println("---------------------------------------------------------");
 
         //TODO 5.- Compara dos arrays recibidos por parámetros devolviendo si son iguales o no
         //Voy a comparar los arrays con el método "equals" de la clase "Arrays", que compara el contenido de los arrays
         System.out.println("5.- Compara dos arrays recibidos por parámetros devolviendo si son iguales o no");
-        System.out.println(Arrays.equals(unArrayParaComparar, otroArrayParaComparar)); //Comparo dos arrays que sé que son iguales, debe de dar TRUE
-        System.out.println(Arrays.equals(arrayAleatorio, unArrayParaComparar)); //Comparo dos arrays que sé que no son iguales, debe de dar FALSE
+        compararArrays(unArrayParaComparar, otroArrayParaComparar);
 
         System.out.println("---------------------------------------------------------");
 
@@ -67,20 +61,46 @@ public class Main {
         System.out.println(Arrays.toString(clonArray));
     }
 
+    //TODO 1.- Método para Crear Array con números aleatorios
 
+    //TODO 2.- Método ordenar invertido
 
+    private static int invertirArray(int[] a) {
+        for (int i = a.length - 1; i > 0; i--) { //Mientras que "i" sea menor que la longitud del array-1 (uno), se hará lo del bucle y se le
+                                                  // restará 1 (uno) a "i", así empezará por la última posición hasta llegar a la primera, la 0 (cero)
+            System.out.print(a[i] + " ");         //Mostramos la posición pertinente
 
+        } return a.length;
+
+    }
+
+    //TODO 3.- Método Ordenar
+    private static String ordenarArray(int[] a)
+    {
+        Arrays.sort(a); //Usando el método "sort" ordenamos arrayAleatorio
+        return Arrays.toString(a);
+    }
 
     //TODO 4.- Método manual
-    private static int buscarEnArray(int[] a, int x) { //Declaramos el método "buscarEnArray", que aceptará dos parámetros, el array en el que queremos buscar
+    private static String buscarEnArray(int[] a, int x) { //Declaramos el método "buscarEnArray", que aceptará dos parámetros, el array en el que queremos buscar
                                                        //y el número que queremos buscar
         for (int i = 0; i < a.length; i++) {           //Hacemos que "i" valga cero y que el bucle se ejecute mientras que "i" sea menor que la longitud del array,
                                                        // reduciendo el valor de i en 1 (uno) en cada iteración
             if (x == a[i]) {                           // En el caso de que el número introducido sea igual al valor de la posición [i]
-                return i;                              //Se devuelve el valor de "i"
-            }
+                return "Búsqueda Manual: El número que buscas está en la posición -> " + i;
+            }                                          //Se devuelve el valor de "i"
         }
-        return -1;                                     //Si no se encuentra, devolverá -1 (uno)
+        return "No se ha encontrado el número";        //Si no se encuentra, devolverá este mensaje
     }
 
+    //TODO 4.- Método Búsqueda Binaria
+    private static String busquedaBinaria(int[]a, int x){
+        return "Búsqueda de binarySearch: El número que buscas está en la posición -> " + Arrays.binarySearch(a,x);
+    }
+
+    //TODO 5.- Compara dos arrays recibidos por parámetros devolviendo si son iguales o no
+    private static void compararArrays(int[] a, int[] b) {
+        System.out.println(Arrays.equals(a, b)); //Este equals es de Arrays
+        System.out.println(a.equals(b)); //Este equals es de Object
+    }
 }
