cab6dba6f189d492b6569413fa96126930624c51
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2023-04-11 23:09:52 +0200

    FileInputStream

---
+++ src/org/ieslosremedios/daw/ud7/ejercicios/actividad_7_1/ejercicio6/EjemploFileInputStream.java
@@ -0,0 +1,28 @@
+package org.ieslosremedios.daw.ud7.ejercicios.actividad_7_1.ejercicio6;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class EjemploFileInputStream {
+        public static void main(String[] args) throws IOException {
+            FileInputStream fis=null;
+            try {
+                fis = new FileInputStream("C:\\Users\\1DAW_2223_01\\Desktop\\Programación\\Unidad-7\\EjemploFileInputStream.txt");
+                int leer=fis.read();
+                while(leer!=-1){
+                    System.out.print((char)leer);
+                    leer=fis.read();
+                }
+                System.out.println();
+            }catch (FileNotFoundException e){
+                System.out.println(e.getMessage());
+            }finally{
+                if (fis!=null){
+                    fis.close();
+                }
+                System.out.println("Programa terminado");
+            }
+        }
+}
+
