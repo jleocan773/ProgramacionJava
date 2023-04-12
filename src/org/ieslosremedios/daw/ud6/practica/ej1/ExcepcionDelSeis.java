167c14117d056a28b23fdf7f10f668ef9d2a34b3
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2023-04-12 20:24:58 +0200

    Add files via upload

---
+++ src/org/ieslosremedios/daw/ud6/practica/ej1/ExcepcionDelSeis.java
@@ -0,0 +1,7 @@
+package org.ieslosremedios.daw.ud6.practica.ej1;
+
+public class ExcepcionDelSeis extends Exception{
+    public ExcepcionDelSeis (String mensaje){
+        super(mensaje);
+    }
+}
