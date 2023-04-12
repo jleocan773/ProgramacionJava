ec9a98b7c50acc04a6c43e7ee3760c4010ca34f4
jleocan773 <115530217+jleocan773@users.noreply.github.com>
2023-04-12 20:22:33 +0200

    Delete src/org/ieslosremedios/daw/ud6/práctica6/ej1 directory

--- src/org/ieslosremedios/daw/ud6/práctica6/ej1/ExcepcionDelSeis.java
+++
@@ -1,7 +0,0 @@
-package org.ieslosremedios.daw.ud6.práctica6.ej1;
-
-public class ExcepcionDelSeis extends Exception{
-    public ExcepcionDelSeis (String mensaje){
-        super(mensaje);
-    }
-}
