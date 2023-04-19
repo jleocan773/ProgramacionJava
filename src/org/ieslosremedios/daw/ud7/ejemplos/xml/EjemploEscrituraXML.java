5a7a6ab63cddfc78a1ab472529762461b7b99d02
Jonathan Leon Canto <jleocan773@g.educaand.es>
2023-04-19 10:34:52 +0200

    XML Ejemplos

---
+++ src/org/ieslosremedios/daw/ud7/ejemplos/xml/EjemploEscrituraXML.java
@@ -0,0 +1,19 @@
+package org.ieslosremedios.daw.ud7.ejemplos.xml;
+
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class EjemploEscrituraXML {
+    public static void main(String[] args) throws ParserConfigurationException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newDefaultInstance();
+        Document document = factory.newDocumentBuilder().newDocument();
+
+
+
+
+
+    }
+}
