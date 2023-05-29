931d86a7e34128b353b5b95f653f392fceff7ff1
Jonathan Leon Canto <jleocan773@g.educaand.es>
2023-05-29 13:48:16 +0200

    Ejemplo Menus

--- src/org/ieslosremedios/daw/ud9/tareas_clase/EjemploMenus.java
+++ src/org/ieslosremedios/daw/ud9/tareas_clase/EjemploMenus.java
@@ -1,32 +1,66 @@
 package org.ieslosremedios.daw.ud9.tareas_clase;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import static org.ieslosremedios.daw.ud8.practica.completa.OperacionesApp.pasarXML_A_Lista;
 
 public class EjemploMenus {
 
         public static void main(String[] args) {
             Frame f= new Frame("Menu and MenuItem Example");
 
-            MenuBar mb=new MenuBar();
+            MenuBar menuBar = new MenuBar();
+            Menu menuArchivo = new Menu("Archivo");
+            Menu menuEditar = new Menu("Editar");
+            MenuItem introducirTexto = new MenuItem("Escribe texto...");
+            MenuItem abrirFichero = new MenuItem("Abre un fichero");
 
-            Menu menuFile=new Menu("File");
-            Menu menuEdit=new Menu("Edit");
-
+            menuArchivo.add(introducirTexto);
+            menuArchivo.add(abrirFichero);
             Menu submenu=new Menu("Sub Menu");
-            MenuItem i1=new MenuItem("Item 1");
-            MenuItem i2=new MenuItem("Item 2");
-            MenuItem i3=new MenuItem("Item 3");
-            MenuItem i4=new MenuItem("Item 4");
-            MenuItem i5=new MenuItem("Item 5");
-            menuFile.add(i1);
-            menuFile.add(i2);
-            menuFile.add(i3);
-            submenu.add(i4);
-            submenu.add(i5);
-            menuFile.add(submenu);
-            mb.add(menuFile);
-            mb.add(menuEdit);
-            f.setMenuBar(mb);
+            Menu paraSubmenu1 = new Menu("Sub Menu 1");
+            Menu paraSubmenu2 = new Menu("Sub Menu 2");
+
+            menuBar.add(menuArchivo);
+            menuBar.add(menuEditar);
+            menuEditar.add(submenu);
+            submenu.add(paraSubmenu1);
+            submenu.add(paraSubmenu2);
+
+
+            //Para la opción "Cargar Lista"
+            introducirTexto.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //Mostrar un cuadro de diálogo de entrada de texto
+                    String texto = JOptionPane.showInputDialog(f, "Introduce un texto:");
+
+                    //Verificar si el usuario ha ingresado un texto
+                    if (!texto.isEmpty()) {
+                        System.out.println(texto);;
+                    }
+                }
+            });
+
+            //Para la opción "abrirFichero"
+            abrirFichero.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser fileChooser = new JFileChooser();
+                    int result = fileChooser.showOpenDialog(f);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        File file = fileChooser.getSelectedFile();
+
+                    }
+                }
+            });
+
+
+            f.setMenuBar(menuBar);
             f.setSize(400,400);
             f.setLayout(null);
             f.setVisible(true);
